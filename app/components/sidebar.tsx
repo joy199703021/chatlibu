de4d99a620e97e8cda5e0a36278941e2ad309588
Andy <130807763+joy199703021@users.noreply.github.com>
2023-06-02 22:57:03 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,10 +119,10 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT</div>
+        <div className={styles["sidebar-title"]}>ChatGPT励步定制版</div>
         <div className={styles["sidebar-sub-title"]}>
-          公众号:施老师的小黑屋
-          （小白屋团队版应急）
+         每一步，自在生长！
+          
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
